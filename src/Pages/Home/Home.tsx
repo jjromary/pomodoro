ec574e114daae260c33ccb3d2c3003a134d3ee4f
joseRbrandao <jose.brandao@softrends.io>
2022-10-26 00:17:46 -0300

    cap02-aula06-Aprimorando-Inputs

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -15,13 +15,27 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput
+            id="task"
+            list="taskSugestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+          <datalist id="taskSugestions">
+            <option value="01" />
+            <option value="A" />
+            <option value="2" />
+            <option value="B" />
+            <option value="José" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             id="minutesAmount"
             type="number"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
