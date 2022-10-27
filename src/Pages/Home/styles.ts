cc8f77706413807596af8dd273d1beae0af6f31e
joseRbrandao <jose.brandao@softrends.io>
2022-10-26 23:53:39 -0300

    countdown created

--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -77,7 +77,7 @@ export const CountdownContainer = styled.div`
 
 export const Separator = styled.div`
     padding: 2rem 0;
-    color: ${(props) => props.theme['gray-500']};
+    color: ${(props) => props.theme['green-500']};
 
     width: 4rem;
     overflow: hidden;
